7318d93b310f0594fd2b1ceeaae01ae8817b2b9f
Ezequiel <eze.etchecoin.92@gmail.com>
2016-08-20 12:29:26 -0300

    Actualizado EntityPersistor

--- MovieMatic/EntityPersistor.cs
+++ MovieMatic/EntityPersistor.cs
@@ -2,41 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace MovieMatic
+namespace Clase22JSon
 {
-    class EntityPersistor<T>
+    public class EntityPersistor<T>
     {
-        private string _path;
+        private string _nombreArchivo;
 
         public EntityPersistor()
         {
-            _path = AppDomain.CurrentDomain.BaseDirectory;
-            
+            var path = AppDomain.CurrentDomain.BaseDirectory;
+            var nombreTipo = typeof(T).FullName;
+            _nombreArchivo = path + "\\" + nombreTipo + ".json";
+        }
+
+        public void Grabar(T entidad)
+        {
+            var lista = new List<T>();
+            lista.Add(entidad);
+            Grabar(lista);
         }
 
         public void Grabar(IEnumerable<T> lista)
         {
-            var listaSerializada = JsonConvert.SerializeObject(lista);
-
-            var nombreTipo = typeof(T).FullName;
-
-            var archivo = _path + "\\" + nombreTipo + ".json";
-
-            System.IO.File.WriteAllText(archivo, listaSerializada);
+            var listaSerializa = JsonConvert.SerializeObject(lista);
+            System.IO.File.WriteAllText(_nombreArchivo, listaSerializa);
         }
+
 
         public IEnumerable<T> Recuperar()
         {
-            var nombreTipo = typeof(T).FullName;
-            var archivo = _path + "\\" + nombreTipo + ".json";
-
-            var contenido = System.IO.File.ReadAllText(archivo);
-            var listaDeserializada = JsonConvert.DeserializeObject<List<T>>(contenido);
-
-            return listaDeserializada;
+            var contenido = System.IO.File.ReadAllText(_nombreArchivo);
+            var lista = JsonConvert.DeserializeObject<List<T>>(contenido);
+            return lista;
         }
     }
 }
